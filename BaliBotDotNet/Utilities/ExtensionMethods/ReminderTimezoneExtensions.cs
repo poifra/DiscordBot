e28a36072eb0bd999ef48e3d13d83f23b5a850e8
Borsti <carstenthormaehlen@hotmail.de>
2026-06-17 19:51:06 +0200

    Added DateReminder to ReminderModule

---
+++ BaliBotDotNet/Utilities/ExtensionMethods/ReminderTimezoneExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static BaliBotDotNet.Modules.ReminderModule;
+
+namespace BaliBotDotNet.Utilities.ExtensionMethods
+{
+    public static class ReminderTimezoneExtensions
+    {
+        public static TimeZoneInfo ToTimeZoneInfo(this ReminderTimezones timezone)
+        {
+            return timezone switch
+            {
+                ReminderTimezones.EST =>
+                    TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time"),
+
+                ReminderTimezones.PST =>
+                    TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time"),
+
+                ReminderTimezones.CEST =>
+                    TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"),
+
+                ReminderTimezones.BST =>
+                    TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"),
+
+                _ => throw new ArgumentOutOfRangeException(
+                    nameof(timezone),
+                    timezone,
+                    "Unsupported timezone.")
+            };
+        }
+    }
+}
